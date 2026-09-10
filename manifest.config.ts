da7d77a22cb1ef3baa4254647b99460fdb0761e9
Loïc Leclercq <lleclercq91@outlook.com>
2026-09-10 11:05:16 +0200

    feat: wire manifest v3 config and vite/crxjs plugin

---
+++ manifest.config.ts
@@ -0,0 +1,24 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+import packageJson from './package.json'
+
+const { version } = packageJson
+
+export default defineManifest({
+  manifest_version: 3,
+  name: 'Tab Launcher',
+  description:
+    "Ouvre des groupes d'onglets pré-configurés par catégorie, en un clic.",
+  version,
+  action: {
+    default_popup: 'src/popup/popup.html',
+  },
+  options_ui: {
+    page: 'src/options/options.html',
+    open_in_tab: true,
+  },
+  background: {
+    service_worker: 'src/background/service-worker.ts',
+    type: 'module',
+  },
+  permissions: ['storage', 'tabs', 'tabGroups'],
+})
